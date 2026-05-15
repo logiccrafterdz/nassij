f301f161bbac3da04bc65be8bef6c697de5cebfc
logiccrafterdz <logiccrafterdz@gmail.com>
2026-05-15 21:14:00 +0100

    fix: Update to V3.0.0, move OCR deps to optional, add integrity to packages

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ Nassij: High-accuracy PDF-to-DOCX converter specialized for Arabic language.
 
 from cli import convert_pdf_to_docx as convert
 
-__version__ = "2.0.0"
+__version__ = "3.0.0"
 __all__ = ["convert"]
